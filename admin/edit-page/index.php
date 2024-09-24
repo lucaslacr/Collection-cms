ee1a1311a60fc0e6664423433816127fce666042
Lucas <39408032+lucaslacr@users.noreply.github.com>
2024-09-24 23:22:13 +0200

    Work on page builder

--- admin/edit-page/index.php
+++ admin/edit-page/index.php
@@ -12,7 +12,12 @@
 </head>
 <body>
     <main>
-        <h1>Edit page {page name}</h1>
+        <nav><a>*back arrow*</a> <a><h1>Edit page {page name}</h1></a> <button>Publish</button></nav>
+        <nav></nav>
+        <section id="composant">Composant layout</section>
+        <section id="">Page area</section>
+        <section>attribut panel</section>
+       
         <form action="#" method="POST">
         <label for="titlepage">Page title</label>
         <input type="text" name="titlepage" id="titlepage" required="" minlength="20" maxlength="60">
@@ -23,7 +28,7 @@
         <input type="checkbox" role="switch" id="robotpage">
         <label for="robotpage"> Page visible for search engine</label>
 
-        <button type="submit">Save changes</button>
+        <button type="submit">Apply</button>
         </form>
     </main>
 </body>
